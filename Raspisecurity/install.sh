968f5e69cfaf5ac14f87de879359ddd65d77be32
oxmc <67136658+oxmc@users.noreply.github.com>
2020-12-01 09:56:00 -0800

    Update install.sh

--- Raspisecurity/install.sh
+++ Raspisecurity/install.sh
@@ -1,6 +1,6 @@
+sudo apt-get install zenity git
 sudo git clone https://github.com/oxmc/raspisecurity.git
 sudo cp -r /home/pi/raspisecurity/Raspisecurity /home/pi/Raspisecurity
 sudo rm -r /home/pi/raspisecurity
 sudo chmod +x /home/pi/Raspisecurity/install2.sh
-sudo apt-get install zenity
 zenity --info --title="Raspisecurity© installer" --text="Part 1 completed!" --ellipsize
